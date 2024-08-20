530a4248f3672974f9baaf8dfb6e32381f53adc2
Роберт <dificalabc@gmail.com>
2024-08-20 18:44:20 +0500

    1

--- module_4_2.py
+++ module_4_2.py
@@ -4,5 +4,5 @@ def test_function():
     return inner_function()
 
 
-# inner_function() не работает
+# inner_function() (NameError: name 'inner_function' is not defined. Did you mean: 'test_function'?)
 test_function()
